dbeb30553db0089320ddaec7072a5820be6bb087
Carla Gonzalez <carlafernanda006@gmail.com>
2024-12-17 17:12:55 -0500

    mejorar

---
+++ imagen.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+imagen = cv2.imread('1.jpg')
+
+
+kernel = np.array([[0, -1, 0], 
+                   [-1, 5, -1], 
+                   [0, -1, 0]])
+
+imagen_nitida = cv2.filter2D(imagen, -1, kernel)
+cv2.imwrite('imagen_mejorada.jpg', imagen_nitida)
+cv2.imshow('Imagen Mejorada', imagen_nitida)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
