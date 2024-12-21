df62c967c9e9fbfa43a41dcf5336d766a8f8f16d
Carla Gonzalez <carlafernanda006@gmail.com>
2024-12-21 17:44:10 -0500

    cambios2

--- EntrenadorPersonal.py
+++ EntrenadorPersonal.py
@@ -1,7 +1,7 @@
 import random
 
-#Proyecto de entrenamiento personal
-#Autora: Carla González Mina
+# Proyecto de entrenamiento personal
+# Autora: Carla González Mina
 
 
 def pedirinformacion():
@@ -46,9 +46,30 @@ def pedirinformacion():
     enlatados = input()
     print("¿Cuántas veces al día comes alimentos integrales?")
     integrales = input()
-    print("¿Cuántas veces al día")
 
-def guardarinfousuario():
+
+def guardarinfousuario(
+    nombre,
+    edad,
+    peso,
+    estatura,
+    objetivo,
+    ejercicio,
+    horas,
+    agua,
+    comidas,
+    frutas,
+    proteinas,
+    carbohidratos,
+    grasas,
+    azucares,
+    lacteos,
+    procesados,
+    fritos,
+    congelados,
+    enlatados,
+    integrales,
+):
     archivo = open("informacionusuario.txt", "w")
     archivo.write(nombre + "\n")
     archivo.write(edad + "\n")
@@ -70,10 +91,124 @@ def guardarinfousuario():
     archivo.write(congelados + "\n")
     archivo.write(enlatados + "\n")
     archivo.write(integrales + "\n")
-    archivo.close()
 
-#La idea es que cada usuario tenga su archivo con su información personal
-#A partir de cada archivo generar entrenamiento adecuado
+
+# La idea es que cada usuario tenga su archivo con su información personal
+(
+    nombre,
+    edad,
+    peso,
+    estatura,
+    objetivo,
+    ejercicio,
+    horas,
+    agua,
+    comidas,
+    frutas,
+    proteinas,
+    carbohidratos,
+    grasas,
+    azucares,
+    lacteos,
+    procesados,
+    fritos,
+    congelados,
+    enlatados,
+    integrales,
+) = pedirinformacion()
+guardarinfousuario(
+    nombre,
+    edad,
+    peso,
+    estatura,
+    objetivo,
+    ejercicio,
+    horas,
+    agua,
+    comidas,
+    frutas,
+    proteinas,
+    carbohidratos,
+    grasas,
+    azucares,
+    lacteos,
+    procesados,
+    fritos,
+    congelados,
+    enlatados,
+    integrales,
+)
+
+# La idea es que cada usuario tenga su archivo con su información personal
+# A partir de cada archivo generar entrenamiento adecuado
+
 
 def calcularentrenamiento(usuario):
-    
+    if usuario == "bajo peso":
+        print("Tu objetivo es subir de peso")
+        print("Tu entrenamiento será el siguiente")
+        print(
+            "Día 1: 30 minutos de cardio, 3 series de 10 repeticiones de sentadillas, 3 series de 10 repeticiones de lagartijas, 3 series de 10 repeticiones de abdominales"
+        )
+        print(
+            "Día 2: 30 minutos de cardio, 3 series de 10 repeticiones de sentadillas, 3 series de 10 repeticiones de lagartijas, 3 series de 10 repeticiones de abdominales"
+        )
+        print(
+            "Día 3: 30 minutos de cardio, 3 series de 10 repeticiones de sentadillas, 3 series de 10 repeticiones de lagartijas, 3 series de 10 repeticiones de abdominales"
+        )
+        print(
+            "Día 4: 30 minutos de cardio, 3 series de 10 repeticiones de sentadillas, 3 series de 10 repeticiones de lagartijas, 3 series de 10 repeticiones de abdominales"
+        )
+        print(
+            "Día 5: 30 minutos de cardio, 3 series de 10 repeticiones de sentadillas, 3 series de 10 repeticiones de lagartijas, 3 series de 10 repeticiones de abdominales"
+        )
+        print(
+            "Día 6: 30 minutos de cardio, 3 series de 10 repeticiones de sentadillas, 3 series de 10 repeticiones de lagartijas, 3 series de 10 repeticiones de abdominales"
+        )
+        print("Día 7: Descanso")
+    elif usuario == "sobrepeso":
+        print("Tu objetivo es bajar de peso")
+        print("Tu entrenamiento será el siguiente")
+        print(
+            "Día 1: 30 minutos de cardio, 3 series de 10 repeticiones de sentadillas, 3 series de 10 repeticiones de lagartijas, 3 series de 10 repeticiones de abdominales"
+        )
+        print(
+            "Día 2: 30 minutos de cardio, 3 series de 10 repeticiones de sentadillas, 3 series de 10 repeticiones de lagartijas, 3 series de 10 repeticiones de abdominales"
+        )
+        print(
+            "Día 3: 30 minutos de cardio, 3 series de 10 repeticiones de sentadillas, 3 series de 10 repeticiones de lagartijas, 3 series de 10 repeticiones de abdominales"
+        )
+        print(
+            "Día 4: 30 minutos de cardio, 3 series de 10 repeticiones de sentadillas, 3 series de 10 repeticiones de lagartijas, 3 series de 10 repeticiones de abdominales"
+        )
+        print(
+            "Día 5: 30 minutos de cardio, 3 series de 10 repeticiones de sentadillas, 3 series de 10 repeticiones de lagartijas, 3 series de 10 repeticiones de abdominales"
+        )
+        print(
+            "Día 6: 30 minutos de cardio, 3 series de 10 repeticiones de sentadillas, 3 series de 10 repeticiones de lagartijas, 3 series de 10 repeticiones de abdominales"
+        )
+        print("Día 7: Descanso")
+    elif usuario == "tonificar":
+        print("Tu objetivo es tonificar")
+        print("Tu entrenamiento será el siguiente")
+        print(
+            "Día 1: 30 minutos de cardio, 3 series de 10 repeticiones de sentadillas, 3 series de 10 repeticiones de lagartijas, 3 series de 10 repeticiones de abdominales"
+        )
+        print(
+            "Día 2: 30 minutos de cardio, 3 series de 10 repeticiones de sentadillas, 3 series de 10 repeticiones de lagartijas, 3 series de 10 repeticiones de abdominales"
+        )
+        print(
+            "Día 3: 30 minutos de cardio, 3 series de 10 repeticiones de sentadillas, 3 series de 10 repeticiones de lagartijas, 3 series de 10 repeticiones de abdominales"
+        )
+        print(
+            "Día 4: 30 minutos de cardio, 3 series de 10 repeticiones de sentadillas, 3 series de 10 repeticiones de lagartijas, 3 series de 10 repeticiones de abdominales"
+        )
+        print(
+            "Día 5: 30 minutos de cardio, 3 series de 10 repeticiones de sentadillas, 3 series de 10 repeticiones de lagartijas, 3 series de 10 repeticiones de abdominales"
+        )
+        print(
+            "Día 6: 30 minutos de cardio, 3 series de 10 repeticiones de sentadillas, 3 series de 10 repeticiones de lagartijas, 3 series de 10 repeticiones de abdominales"
+        )
+        print("Día 7: Descanso")
+    else:
+        print("No se ha encontrado un entrenamiento adecuado")
